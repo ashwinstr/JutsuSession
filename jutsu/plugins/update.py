9d83ca737a0ea8c0b86c35b7fb6ffb77fc8c8433
Kakashi <ashwinstr@gmail.com>
2021-12-09 19:39:26 +0530

    added updater

---
+++ jutsu/plugins/update.py
@@ -0,0 +1,37 @@
+import heroku3
+import os
+import asyncio
+import time
+
+from pyrogram import Client, filters
+from git import Repo
+from git.exc import GitCommandError
+
+
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_ENV = True
+HEROKU_APP = (
+        heroku3.from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME]
+        if HEROKU_ENV and HEROKU_API_KEY and HEROKU_APP_NAME
+        else None
+    )
+
+
+@Client.on_message(
+    filters.command(["update"], prefixes="$")
+    & filters.user([1013414037])
+    & filters.group,
+    group=0
+)
+async def updater_(bot, message):
+    if HEROKU_APP:
+        msg_ = await bot.send_message(
+            message.chat.id,
+            "`Restarting the bot...\nIt will take upto 10 sec to update.`",
+        )
+        HEROKU_APP.restart()
+        time.sleep(10)
+    else:
+        await bot.send_message(message.chat.id, "`Restarting [HARD] ...`")
+        asyncio.get_event_loop().create_task(bot.restart(hard=True))
