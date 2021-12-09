aee81d4f14fcdcd6253e52a0650a65095f48f9ff
Kakashi <ashwinstr@gmail.com>
2021-12-09 19:45:58 +0530

    bug fix

--- jutsu/tools/conversation.py
+++ jutsu/tools/conversation.py
@@ -1,13 +1,13 @@
-import time
+
 import asyncio
 
 from typing import Union
 
-from pyrogram import filters, Client
 
 async def conv(bot, chat_id: Union[int, str], msg_id: int, user_id: int):
     while True:
         await asyncio.sleep(0.1)
+        resp = None
         try:
             resp = await bot.get_messages(chat_id, (msg_id + 1))
             await bot.send_read_acknowledge(chat_id, (msg_id + 1))
