666ecab776550f6e7b54f99d4425e0e81a7e61a1
Kakashi <ashwinstr@gmail.com>
2021-12-09 21:25:30 +0530

    testing

--- jutsu/tools/conversation.py
+++ jutsu/tools/conversation.py
@@ -4,14 +4,20 @@ import asyncio
 from typing import Union
 
 
-async def conv(bot, chat_id: Union[int, str], msg_id: int, user_id: int):
+async def converse(bot, chat_id: Union[int, str], msg_id: int, user_id: int):
     resp = None
-    while True:
+    while resp is None:
         await asyncio.sleep(0.5)
         try:
             resp = await bot.get_messages(chat_id, int(msg_id + 1))
         except:
-            pass
-        if resp is not None:
-            break
+            resp = None
     return resp
+
+
+async def conv(bot, chat_id, msg_id, user_id):
+    try:
+        response = await asyncio.wait_for(converse(bot, chat_id, msg_id, user_id), timeout=15)
+        return response
+    except asyncio.TimeoutError:
+        return "Timeout"
