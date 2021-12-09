ed58749936d6ee128b85bd485bc58ed83539b17f
Kakashi <ashwinstr@gmail.com>
2021-12-09 19:37:06 +0530

    tb

--- jutsu/plugins/session.py
+++ jutsu/plugins/session.py
@@ -9,26 +9,30 @@ from jutsu import conv
     & filters.private
 )
 async def session_(bot, message):
-    start_id = await bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}, let's start with string generation with you replying your APP_ID to this message.")
-    app_id = await conv(
-        bot,
-        chat_id=message.chat.id,
-        msg_id=start_id.message_id,
-        user_id=message.from_user.id,
-    )
-    APP_ID = app_id.text
-    if not isinstance(APP_ID, int) or len(APP_ID):
-        return await bot.send_message(message.chat.id, "The APP_ID must be a 7 digit number. Please try again with /start.")
-    start_hash = await bot.send_message(message.chat.id, "APP_ID received, now send API_HASH.")
-    api_hash = await conv(
-        bot,
-        chat_id=message.chat.id,
-        msg_id=start_hash.message_id,
-        user_id=message.from_user.id,
-    )
-    API_HASH = api_hash.text
     try:
-        with Client(":memeory:", api_id=int(APP_ID), api_hash=API_HASH) as app:
-            app.send_message("me", f"#HU_STRING_SESSION generated successfully.\n\n```{app.export_session_string()}```\n\n#KEEP_IT_SAFE.")
+        start_id = await bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}, let's start with string generation with you replying your APP_ID to this message.")
+        app_id = await conv(
+            bot,
+            chat_id=message.chat.id,
+            msg_id=start_id.message_id,
+            user_id=message.from_user.id,
+        )
+        APP_ID = app_id.text
+        if not isinstance(APP_ID, int) or len(APP_ID):
+            return await bot.send_message(message.chat.id, "The APP_ID must be a 7 digit number. Please try again with /start.")
+        start_hash = await bot.send_message(message.chat.id, "APP_ID received, now send API_HASH.")
+        api_hash = await conv(
+            bot,
+            chat_id=message.chat.id,
+            msg_id=start_hash.message_id,
+            user_id=message.from_user.id,
+        )
+        API_HASH = api_hash.text
+        try:
+            with Client(":memeory:", api_id=int(APP_ID), api_hash=API_HASH) as app:
+                app.send_message("me", f"#HU_STRING_SESSION generated successfully.\n\n```{app.export_session_string()}```\n\n#KEEP_IT_SAFE.")
+        except:
+            await bot.send_message(message.chat.id, "Something went wrong, make sure your inputs are correct. If they are correct, since this bot is in beta phase, please report to @UX_xplugin_support.")
     except:
-        await bot.send_message(message.chat.id, "Something went wrong, make sure your inputs are correct. If they are correct, since this bot is in beta phase, please report to @UX_xplugin_support.")
+        tb = traceback.format_exc()
+        await bot.send_message(-1001661347032, f"#SESSION_BOT\n\n```{tb}```")
