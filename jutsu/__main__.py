d1b8234776e9b9c09af04e199cd76d89413e7af0
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-12-09 17:49:35 +0530

    Create __main__.py

---
+++ jutsu/__main__.py
@@ -0,0 +1,22 @@
+import pyrogram
+from pyrogram import Client, filters
+from decouple import config
+
+
+APP_ID = config("APP_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+
+
+if __name__ == "__main__" :
+    print("### Starting Bot... ###")
+    plugins = dict(root="jutsu/plugins")
+    app = pyrogram.Client(
+        "JutsuSessions",
+        bot_token=BOT_TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        
+        plugins=plugins
+    )
+    app.run()
