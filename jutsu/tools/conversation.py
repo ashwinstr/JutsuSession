4f62dd3cced823e77f115181a83fb5b2fb6df4a6
Kakashi <ashwinstr@gmail.com>
2021-12-09 21:12:10 +0530

    testing

--- jutsu/tools/conversation.py
+++ jutsu/tools/conversation.py
@@ -12,6 +12,6 @@ async def conv(bot, chat_id: Union[int, str], msg_id: int, user_id: int):
             resp = await bot.get_messages(chat_id, int(msg_id + 1))
         except:
             pass
-        if resp:
+        if resp is not None:
             break
     return resp
