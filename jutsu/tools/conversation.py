d6cdc4b70fa5ea0c790fbd66b7415798b9e4ece5
Kakashi <ashwinstr@gmail.com>
2021-12-09 21:32:59 +0530

    update

--- jutsu/tools/conversation.py
+++ jutsu/tools/conversation.py
@@ -2,6 +2,9 @@
 import asyncio
 
 from typing import Union
+from telegraph import Telegraph
+
+tele_ = Telegraph()
 
 
 async def converse(bot, chat_id: Union[int, str], msg_id: int, user_id: int):
@@ -17,7 +20,35 @@ async def converse(bot, chat_id: Union[int, str], msg_id: int, user_id: int):
 
 async def conv(bot, chat_id, msg_id, user_id):
     try:
-        response = await asyncio.wait_for(converse(bot, chat_id, msg_id, user_id), timeout=15)
+        response = await asyncio.wait_for(await converse(bot, chat_id, msg_id, user_id), timeout=15)
         return response
     except asyncio.TimeoutError:
         return "Timeout"
+
+
+def telegrapher(a_title: str, content: str) -> str:
+    auth_name = tele_.create_account(short_name="Kakashi")
+    resp = tele_.create_page(
+        title=a_title,
+        author_name=auth_name,
+        author_url="https://t.me/xplugin",
+        html_content=content,
+    )
+    link_ = resp["url"]
+    return link_
+
+
+def int_list(list_):
+    intlist = []
+    for one in list_:
+        if one.isdigit():
+            one = int(one)
+        intlist.append(one)
+    return intlist
+
+async def username_list(list_):
+    u_list = []
+    for one in list_:
+        u_list.append((await bot.get_users(one)).username)
+    return u_list
+
