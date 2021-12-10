26bca3e73f90d650cb47fff20f2002b091b709e1
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-12-10 10:25:21 +0530

    Update session.py

--- jutsu/plugins/session.py
+++ jutsu/plugins/session.py
@@ -90,8 +90,8 @@ async def api_hash_(bot, message):
 )
 async def generate_(bot, message):
     try:
-        with Client(":memeory:", api_id=int(Config.APP_ID), api_hash=Config.API_HASH) as app:
-            await app.send_message(message.from_user.id, f"#HU_STRING_SESSION generated successfully.\n\n```{await app.export_session_string()}```\n\n#KEEP_IT_SAFE.")
+        async with Client(":memeory:", api_id=int(Config.APP_ID), api_hash=Config.API_HASH) as jutsu:
+            await jutsu.send_message(message.from_user.id, f"#HU_STRING_SESSION generated successfully.\n\n```{await jutsu.export_session_string()}```\n\n#KEEP_IT_SAFE.")
     except:
         await bot.send_message(message.chat.id, "Something went wrong, make sure your inputs are correct. If they are correct, since this bot is in beta phase, please report to @UX_xplugin_support.")
         tb = traceback.format_exc()
